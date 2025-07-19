006ed6713943d97be73eb595b97f3105a05c29bd
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 <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>
2025-07-19 12:00:39 +0100

    printing error messahe instead of returning it

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -8,13 +8,14 @@ printRmCharacters() async{
     try{
       parsed_url = Uri.parse("https://rickandmortyapi.com/api/character?page=${i}");
       data = await http.get(parsed_url);
+      data = jsonDecode(data.body)["results"];
+      for (var characterInfo in data){
+        print(characterInfo["name"]);
+      }
     }
     catch(e){
-      return("error caught: $e");
-    }
-    data = jsonDecode(data.body)["results"];
-    for (var characterInfo in data){
-      print(characterInfo["name"]);
+      print("error caught: $e");
+      return;
     }
   }  
 }
