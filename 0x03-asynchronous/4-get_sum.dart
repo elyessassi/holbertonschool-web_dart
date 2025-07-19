24ee5ce4214ad9e1a83def0f61deeaf3dca59ad1
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 <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>
2025-07-19 11:31:12 +0100

    improving error handling

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -5,27 +5,16 @@ import '4-util.dart';
 Future<num> calculateTotal() async{
   var userID, response, orders;
   double Total = 0;
-  
   try{
     response = await fetchUserData();
-  }
-  catch(e){
-    return(-1);
-  }
-  userID = jsonDecode(response)["id"];
-  try{
+    userID = jsonDecode(response)["id"];
     orders = jsonDecode(await fetchUserOrders(userID));
-  }
-  catch(e){
-    return(-1);
-  }
-  for(var i = 0; i < orders.length; i++ ){
-    try{
+    for(var i = 0; i < orders.length; i++ ){
       Total = Total + jsonDecode(await fetchProductPrice(orders[i]));
     }
-    catch(e){
-      return(-1);
-    }
+    return(Total);
   }
-  return(Total);
-}
+  catch(e){
+    return(-1);
+  }
+}  
