2e457e1efbc9f09292a64d4b0f69cbab304f0f3e
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 <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>
2025-07-19 12:11:18 +0100

    showing only one page

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -4,18 +4,15 @@ import 'package:http/http.dart' as http;
 
 printRmCharacters() async{
   var data, parsed_url;
-  for (var i = 1; i < 43; i++){
-    try{
-      parsed_url = Uri.parse("https://rickandmortyapi.com/api/character?page=${i}");
-      data = await http.get(parsed_url);
-      data = jsonDecode(data.body)["results"];
-      for (var characterInfo in data){
-        print(characterInfo["name"]);
-      }
+  try{
+    parsed_url = Uri.parse("https://rickandmortyapi.com/api/character?page=${i}");
+    data = await http.get(parsed_url);
+    data = jsonDecode(data.body)["results"];
+    for (var characterInfo in data){
+      print(characterInfo["name"]);
     }
-    catch(e){
-      print("error caught: $e");
-      return;
-    }
-  }  
+  }
+  catch(e){
+    return("error caught: $e");
+  }
 }
